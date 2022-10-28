44f098bcd378f23266bb172ed84ca9a8c49714be
MrQureshi-xd <64306867+Mr-Qureshi-xd@users.noreply.github.com>
2022-10-29 01:21:06 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,18 @@
 from os import system
+import time
 from platform import machine
 print('Checking For Update...')
 system('git pull')
 print('Subcrib My Channel First...')
 os.system('xdg-open https://youtube.com/c/MrQureshiTech')
 time.sleep(2)
-prinnt('Join Our Facebook Group For More Update bruh...')
+print('Join Our Facebook Group For More Update bruh...')
 os.system('xdg-open https://facebook.com/groups/447671328737321/')
 if machine()=='aarch64':
-    os.system('chmod 777 nox.so')
+    
     import nox.so
     nox.main
 else:
-    os.system('chmod 777 nox32.so')
+    
     import nox32.so
     nox.main
