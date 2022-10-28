e5e7e114ed1619f5e9df712fdbc859e9c7774abd
MrQureshi-xd <64306867+Mr-Qureshi-xd@users.noreply.github.com>
2022-10-29 01:44:24 +0500

    Update run.py

--- run.py
+++ run.py
@@ -9,5 +9,9 @@ os.system('xdg-open https://youtube.com/c/MrQureshiTech')
 time.sleep(2)
 print('Join Our Facebook Group For More Update bruh...')
 os.system('xdg-open https://facebook.com/groups/447671328737321/')
-import nox
-nox.main()
+if machine()=='aarch64':
+    import nox
+    nox.main()
+else:
+    import nox32
+    nox32.main()
