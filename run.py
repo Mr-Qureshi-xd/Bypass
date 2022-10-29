8789db9ba1ad4794ff28b62c06b4a508e3a15ed7
MrQureshi-xd <64306867+Mr-Qureshi-xd@users.noreply.github.com>
2022-10-29 12:25:01 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-
+'''Coded By Mr Qureshi'''
 from os import system
 import time
 import os
@@ -7,6 +7,9 @@ print('Checking For Update...')
 system('git pull')
 print('Subcrib My Channel First...')
 os.system('xdg-open https://youtube.com/c/MrQureshiTech')
+time.sleep(2)
+print('Subcrib My Channel First...')
+os.system('xdg-open https://youtube.com/channel/UCyUzwD8WYuICl5xf4CGZA5w')
 time.sleep(2)
 print('Join Our Facebook Group For More Update bruh...')
 os.system('xdg-open https://facebook.com/groups/447671328737321/')
